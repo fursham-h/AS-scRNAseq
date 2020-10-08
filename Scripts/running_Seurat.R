6193ae9a3b03ca0609b75289eab67cffc42b38f3
Fursham Hamid <fursham.h@gmail.com>
2020-10-08 15:15:52 +0100

    Update filename

--- Scripts/running_Seurat.R
+++ Scripts/running_Seurat.R
@@ -1,16 +1,24 @@
 # Step 1: import dataset into R
 
+
 # Step 2: re-annotate MT genes
 # I will assist with this 
 
+
 # Step 3: Create Seurat object 
+
 
 # Step 4: Perform QC on dataset and subset data further 
 
+
 # Step 5: Normalize, identify highly variable features and scale data 
+
 
 # Step 6: carry out linear dimensional reduction
 
+
 # Step 7: Cluster cells
 
+
 # Step 8: Perform non-linear dimensional reduction 
+

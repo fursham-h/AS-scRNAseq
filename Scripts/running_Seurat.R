832133307138bb179fdcc95f6e938f17ca630aaf
“Fursham” <fursham.h@gmail.com>
2020-10-09 09:33:06 +0100

    Modified script

--- Scripts/running_Seurat.R
+++ Scripts/running_Seurat.R
@@ -1,40 +1,41 @@
-# Step 1: import dataset into R
+# Step 1: import dataset into R as matrix
+# hint: a matrix should contain only count integers.
+## The first row of the interneuron data has column names, 
+## and the first column of the data has feature names.
+## We need to import the data in such a way that the first row will be converted to column names
+## and the first column converted into row names
+input.matrix <- # complete this code
 
 
-# Step 2: re-annotate MT genes
-# I will assist with this 
 
-# Step 2.1: download mouse reference genome
-# firstly, we need to install a package that contain genome databases 
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install("AnnotationHub")
-
-# load mouse genome
-library(AnnotationHub) 
-ah <- AnnotationHub()
-
-# retrieve latest GRCm38 annotation from ensembl
-mouse_genome <- ah[['AH60127']]
-
-# Step 2.2: prepare dataframe containing a list of gene-if, gene_name and chromosome origin
-mouse_genes <- distinct(data.frame(gene_id = mouse_genome$gene_id, gene_name = mouse_genome$gene_name, chromosome = as.character(seqnames(mouse_genome)))) 
-#to be continued
-
-# Step 3: Create Seurat object 
+# Step 2: Create Seurat object 
 
 
-# Step 4: Perform QC on dataset and subset data further 
+  
+  
+# Step 3: Perform QC on dataset and subset data further 
+## I just realize that MT genes have been annotated in the list of features.
+## However, it is named like this "ENSMUSG00000064336-MT-TF"
+## Therefore, the pattern input for PercentageFeatureSet function will be different
 
 
-# Step 5: Normalize, identify highly variable features and scale data 
+
+# Step 4: Normalize, identify highly variable features and scale data 
 
 
-# Step 6: carry out linear dimensional reduction
 
 
-# Step 7: Cluster cells
+# Step 5: carry out linear dimensional reduction
 
 
-# Step 8: Perform non-linear dimensional reduction 
+
+
+# Step 6: Cluster cells
+
+
+
+
+# Step 7: Perform non-linear dimensional reduction 
+
+
 

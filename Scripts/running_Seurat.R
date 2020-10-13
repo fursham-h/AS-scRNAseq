95e7edd5458725b42d6d5d55442f6877f87859ef
Cassy-Le <71974352+Cassy-Le@users.noreply.github.com>
2020-10-13 16:10:34 +0200

    Update running_Seurat.R

--- Scripts/running_Seurat.R
+++ Scripts/running_Seurat.R
@@ -4,15 +4,19 @@
 ## and the first column of the data has feature names.
 ## We need to import the data in such a way that the first row will be converted to column names
 ## and the first column converted into row names
-input.matrix <- # complete this code
+input.matrix <- read.table("GSE109796_Oscar.GEO.singleCell.gene.count.txt", header=TRUE, sep=" ")
 
 
 
 # Step 2: Create Seurat object 
 
+mydata <- CreateSeuratObject(counts = input.matrix, min.cells = 3, min.genes = 200, project = "interneuron")
+  
+  Error in CreateAssayObject(counts = counts, min.cells = min.cells, min.features = min.features) : 
+  No feature names (rownames) names present in the input matrix
+In addition: Warning message:
+In storage.mode(from) <- "double" : NAs introduced by coercion
 
-  
-  
 # Step 3: Perform QC on dataset and subset data further 
 ## I just realize that MT genes have been annotated in the list of features.
 ## However, it is named like this "ENSMUSG00000064336-MT-TF"
